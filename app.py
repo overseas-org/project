a375fd751840eb4958ad3d696e70ff6654d1dbe2
hanna <channashosh@gmail.com>
2025-09-04 08:30:17 +0300

    first

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask, request, jsonify
+from flask import Flask
+from flask_cors import CORS
+from crud_mysql import crud
 
 app = Flask(__name__)
 
+CORS(app, supports_credentials=True)
+
+app.register_blueprint(crud)
+
 if __name__ == "__main__":
-	app.run(host="0.0.0.0", debug=True)
+	app.run(host="0.0.0.0", debug=True, port=5002)
