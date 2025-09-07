66b5ecce1a3877ed699c97135149688a335ed6b2
hanna <channashosh@gmail.com>
2025-09-07 12:12:27 +0300

    crud project

--- app.py
+++ app.py
@@ -1,31 +1,12 @@
-from flask import Flask, request, jsonify
+from flask import Flask
+from flask_cors import CORS
+from crud_mysql import crud
 
 app = Flask(__name__)
 
-@app.route("/projects", methods=["GET"])
-def get projects():
-	data = request.json
-	return jsonify(f"reply from endpoint get projects, data = {data}")
+CORS(app, supports_credentials=True)
 
-@app.route("/project", methods=["GET"])
-def get project():
-	data = request.json
-	return jsonify(f"reply from endpoint get project, data = {data}")
-
-@app.route("/project", methods=["DELETE"])
-def delete project():
-	data = request.json
-	return jsonify(f"reply from endpoint delete project, data = {data}")
-
-@app.route("/project", methods=["PUT"])
-def update project():
-	data = request.json
-	return jsonify(f"reply from endpoint update project, data = {data}")
-
-@app.route("/project", methods=["POST"])
-def create project():
-	data = request.json
-	return jsonify(f"reply from endpoint create project, data = {data}")
+app.register_blueprint(crud)
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0", debug=True)
+	app.run(host="0.0.0.0", debug=True, port=5002)
