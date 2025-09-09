82132294ce637119ddb68b766640d9a0a6f9eee0
hanna <channashosh@gmail.com>
2025-09-09 04:48:54 +0300

    test db

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_cors import CORS
 from crud_mysql import crud
+from mysql_database import Database, DatabaseCreds
+import os, logging
 
 app = Flask(__name__)
 
@@ -9,4 +11,12 @@ CORS(app, supports_credentials=True)
 app.register_blueprint(crud)
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0", debug=True, port=5002)
+	try:
+		db = Database("project", DatabaseCreds(os.environ("DATABASE_HOST"), os.environ("DATABASE_USER"), os.environ("DATABASE_PASSWORD")))
+		if not db:
+			logging.info("db connected")
+		else:
+			logging.error("no db found")
+	except:
+		logging.error("failed to connect")
+	app.run(host="0.0.0.0", debug=True, port=5000)
